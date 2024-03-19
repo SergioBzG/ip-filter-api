beb1013bf6868f6c20c8d11df5375b4a45538707
sergio <sebermudezg@unal.edu.co>
2024-03-19 12:40:17 -0500

    Removed smell code

--- src/main/java/com/sbz/ipfilter/application/validator/IpChecker.java
+++ src/main/java/com/sbz/ipfilter/application/validator/IpChecker.java
@@ -1,11 +1,6 @@
 package com.sbz.ipfilter.application.validator;
 
-import com.sbz.ipfilter.application.dto.RuleDto;
 import org.springframework.validation.BindingResult;
-
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
 
 public interface IpChecker {
     void checkInvalidOrMissingData(BindingResult errors);
